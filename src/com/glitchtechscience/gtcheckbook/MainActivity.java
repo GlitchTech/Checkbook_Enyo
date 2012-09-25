43266a34f40427c15140562dbe8d63f63b2e30b2
Matt Schott <ryltar@ku.edu>
2012-09-25 17:15:36 -0500

    Added Checkbook (enyo2) submodule

--- src/com/glitchtechscience/gtcheckbook/MainActivity.java
+++ src/com/glitchtechscience/gtcheckbook/MainActivity.java
@@ -1,20 +1,17 @@
 package com.glitchtechscience.gtcheckbook;
 
+import org.apache.cordova.DroidGap;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		super.onCreate( savedInstanceState );
+		
+		//super.loadUrl( "file:///android_asset/www/index.html" );
+		super.loadUrl( "file:///android_asset/www/debug.html" );
+	}
 }
