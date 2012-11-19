a7be24dfcdbf067ec359ff6fe9b1bfc0f114e80c
Matt Schott <ryltar@ku.edu>
2012-11-19 16:25:55 -0600

    submodule update (nonfunct deploy)

--- source/package.js
+++ source/package.js
@@ -1,37 +1,6 @@
 /* Copyright © 2011-2012, GlitchTech Science */
 
 enyo.depends(
-	"$lib/g11n",
-	"$lib/layout",
-
-	//To theme Onyx using Theme.less, swap commented lines and follow the steps described in Theme.less
-	"$lib/onyx",
-	//"$lib/onyx/source",
-	//"Theme.less",
-
-	"$lib/canvas-spinner/heartcode_canvas.js",
-	"$lib/canvas-spinner/canvasSpinner.js",
-
-	"$lib/aes.js",
-
-	"$lib/gts-plugins/PrototypeExt",
-	"$lib/gts-plugins/GAPI",
-	"$lib/gts-plugins/LazyList",
-	"$lib/gts-plugins/SelectorBar",
-	"$lib/gts-plugins/ToggleBar",
-	"$lib/gts-plugins/Divider",
-	"$lib/gts-plugins/DividerDrawer",
-	"$lib/gts-plugins/SelectedMenu",
-	"$lib/gts-plugins/InlineNotification",
-	"$lib/gts-plugins/DatePicker",
-	"$lib/gts-plugins/TimePicker",
-	"$lib/gts-plugins/DecimalInput",
-	"$lib/gts-plugins/ConfirmDialog",
-	"$lib/gts-plugins/ProgressDialog",
-	"$lib/gts-plugins/AutoComplete",
-
-	"$lib/private",
-
 	"scripts/",
 	"styles/"
 );
