04237605d91835aba283a14566826aaa589737e6
Matt Schott <ryltar@ku.edu>
2012-12-11 08:57:44 -0600

    DB update command

--- source/scripts/splash.js
+++ source/scripts/splash.js
@@ -159,10 +159,8 @@ enyo.kind({
 			currVersion = results[0]['dbVer'];
 		}
 
-		this.log( currVersion );
-
 		//DB Version
-		this.versionCheck = 24;
+		this.versionCheck = 25;
 
 		if( currVersion == this.versionCheck ) {
 
@@ -794,11 +792,17 @@ enyo.kind({
 
 				this.versionCheck = 24;
 			case 24:
-				//this.versionCheck = 25;
+				//Payee field option
+				querySet.push( "ALTER TABLE accounts ADD COLUMN payeeField INTEGER NOT NULL DEFAULT 0;" );
+				querySet.push( "ALTER TABLE transactions ADD COLUMN payee TEXT;" );
 
+				this.versionCheck = 25;
+			case 25:
 				//GTS Sync System
 				//querySet.push( "DROP TABLE IF EXISTS syncQueue;" );
 				//querySet.push( "CREATE TABLE syncQueue( syncId INTEGER PRIMARY KEY ASC, action TEXT, table TEXT, data TEXT, where TEXT, ts INTEGER, sourceTable TEXT, sourceId INTEGER );" );
+
+				//this.versionCheck = 26;
 		}
 
 		querySet.push(
